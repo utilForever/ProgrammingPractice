971869405567b4b44009a8ca0ff2f46cf93e7587
Kamikoto <Kamikoto66@gmail.com>
2017-05-27 15:37:13 +0900

    GameShop_1 과제제출

---
+++ Solutions/Kamikoto66/GameShop1/Item.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+class Item
+{
+private:
+	string _name;
+	string _description;
+	int _weight;
+	int _value;
+
+public:
+	Item();
+	Item(string name, string description, int weight, int value);
+	~Item();
+
+	void Describe();
+};
+
