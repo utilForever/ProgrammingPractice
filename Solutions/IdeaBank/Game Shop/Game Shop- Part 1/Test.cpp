901e748297afb8fec0797b40f3a7442075525676
IdeaBank <isp_lwj@naver.com>
2017-05-27 15:50:55 +0900

    Update Test.cpp

--- Solutions/IdeaBank/Game Shop/Game Shop- Part 1/Test.cpp
+++ Solutions/IdeaBank/Game Shop/Game Shop- Part 1/Test.cpp
@@ -1,10 +1,8 @@
-#include"ShopClass.h"
-
 int main()
 {
-	Item* item = new Weapon("Excalibur", "The legendary sword of King Arthur", 12, 1024, 24);
-	item->Describe();
-
-	item = new Armor("Steel Armor", "Protective covering made by steel", 15, 805, 18);
-	item->Describe();
+	Item *a[2];
+	a[0] = new Item("Excalibur", "The legendary sword of King Arthur", 12, 1024);
+	a[1] = new Item("Steel Armor", "Protective covering made by steel", 15, 805);
+	a[0]->Describe();
+	a[1]->Describe();
 }
