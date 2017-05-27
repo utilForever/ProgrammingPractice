a1f20092da33999b339d508fde426201d9e8e4ee
IdeaBank <isp_lwj@naver.com>
2017-05-27 15:04:18 +0900

    Add files via upload

---
+++ Solutions/Woojin/Game Shop/GameShop_1.cpp
@@ -0,0 +1,11 @@
+#include"ShopClass.h"
+
+int main()
+{
+	//Test
+	Item *a[2];
+	a[0] = new Item("Excalibur", "The legendary sword of King Arthur", 12, 1024);
+	a[1] = new Item("Steel Armor", "Protective covering made by steel", 15, 805);
+	a[0]->Describe();
+	a[1]->Describe();
+}
---
+++ Solutions/Woojin/Game Shop/ShopClass.h
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+
+class Item{
+public:
+	Item() = default;
+	Item(std::string, std::string, int, int);
+	void Describe();
+protected:
+	std::string name;
+	std::string description;
+	int weight;
+	int value;
+};
+Item::Item(std::string name, std::string description, int weight, int value)
+{
+	this->name = name;
+	this->description = description;
+	this->weight = weight;
+	this->value = value;
+}
+void Item::Describe()
+{
+	std::cout << "Name\t\t= " << this->name << std::endl;
+	std::cout << "Description\t= " << this->description << std::endl;
+	std::cout << "Weight\t\t= " << this->weight << " lbs" << std::endl;
+	std::cout << "Value\t\t= " << this->value << " gold coins" << std::endl << std::endl;
+}
