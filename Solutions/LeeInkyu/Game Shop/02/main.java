d1a57f52e9c688d61ca13eb662b88cf37ced9202
Inkyu Lee <123inn9@naver.com>
2017-05-29 08:53:04 +0900

    Add files via upload

--- Solutions/LeeInkyu/Game Shop/02/main.java
+++ Solutions/LeeInkyu/Game Shop/02/main.java
@@ -4,10 +4,10 @@
 public class main {
     public static void main(String args[])
     {
-        Weapon Excalibur = new Weapon("Excalibur", "The legendary sword of King Arthur", 12, 1024, 24);
-        Armor SteelArmor = new Armor("Steel Armor", "Protective covering made by steel", 15, 805, 18);
+        Item Excalibur = new Weapon("Excalibur", "The legendary sword of King Arthur", 12, 1024, 24);
+        Item SteelArmor = new Armor("Steel Armor", "Protective covering made by steel", 15, 805, 18);
 
-        Excalibur.Describe();
-        SteelArmor.Describe();
+        Excalibur.describe();
+        SteelArmor.describe();
     }
 }
