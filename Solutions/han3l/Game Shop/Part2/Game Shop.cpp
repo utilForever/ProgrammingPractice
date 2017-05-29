11de9498dd48c6f2066937e11426cb998554be7e
Han3l <Han3l@users.noreply.github.com>
2017-05-30 08:12:42 +0900

    Changed "Amor" to "Armor"

--- Solutions/han3l/Game Shop/Part2/Game Shop.cpp
+++ Solutions/han3l/Game Shop/Part2/Game Shop.cpp
@@ -5,7 +5,7 @@ int main()
 	Item* item = new Weapon("Excalibur", "The legendart sword of King Arthur", 12, 1204, 24);
 	item->Describe();
 	
-	item = new Amor("Steel Armor", "Protective covering made by steel", 15, 805, 18);
+	item = new Armor("Steel Armor", "Protective covering made by steel", 15, 805, 18);
 	item->Describe();
 	
 	delete item;
