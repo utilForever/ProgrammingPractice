83922b3d9ad6022dde559f6fc5ddcf6ddb373059
Seonggwon Yoon <keyakoto@gmail.com>
2017-06-14 03:07:27 +0900

    change tab to PEP standard

--- Solutions/keyakoto/Game Shop/Part1/gameshop1.py
+++ Solutions/keyakoto/Game Shop/Part1/gameshop1.py
@@ -1,16 +1,16 @@
 class Item: # python3
-	def __init__(self, name, description, weight, value):
-		self.name = name
-		self.description = description
-		self.weight = weight
-		self.value = value
+    def __init__(self, name, description, weight, value):
+        self.name = name
+        self.description = description
+        self.weight = weight
+        self.value = value
 
-	def Desribe(self):
-		msg = "Name		= " + self.name + "\n"
-		msg += "Description = " + self.description + "\n"
-		msg += "Weight      = " + self.weight + "\n"
-		msg += "Value       = " + self.value + "\n" 
-		return msg
+    def Desribe(self):
+        msg = "Name		= " + self.name + "\n"
+        msg += "Description = " + self.description + "\n"
+        msg += "Weight      = " + self.weight + "\n"
+        msg += "Value       = " + self.value + "\n"
+        return msg
 
 myItem = Item("Excalibur", "The legendary sword of King Arthur", "12 lbs", "1024 gold coins")
 print (myItem.Desribe())
