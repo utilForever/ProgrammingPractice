c42975612497b50976b75def49d08d2371321569
maxlee <max1397375155@gmail.com>
2017-07-11 19:27:42 +0900

    [Game Shop] Complete Part 1

---
+++ Solutions/maaaaaaaax12/Game Shop/Part 1/Shop.cpp
@@ -0,0 +1,19 @@
+#include "Shop.h"
+
+#include <iostream>
+#include <iomanip>
+
+Item::Item(string name, string description, int weight, int value)
+	:name(name), description(description), weight(weight), value(value)
+{
+
+}
+
+void Item::Describe()
+{
+	cout.setf(ios::left);
+	cout << setw(13) << "Name" << "= " << name << endl;
+	cout << setw(13) << "Description" << "= " << description << endl;
+	cout << setw(13) << "Weight" << "= " << weight << endl;
+	cout << setw(13) << "Value" << "= " << value << endl;
+}
